534f57b61ccb098b23cd97d49c459d266aad2310
Alex Jones <alex.jones@L01798.local>
2019-01-04 14:56:24 +0000

    salesforce link

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const http = require('http')
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//Display a list of all the account in the org
+app.get('/', function(req, res) {
+    res.sendFile('views/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
